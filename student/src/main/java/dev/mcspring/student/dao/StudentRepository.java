b780b397fd32cae9c476b85e4f0a2ee962d24df7
vitalvirtue <hyterdem@hotmail.com>
2024-06-07 11:50:11 +0300

    Student repository

---
+++ student/src/main/java/dev/mcspring/student/dao/StudentRepository.java
@@ -0,0 +1,11 @@
+package dev.mcspring.student.dao;
+
+import dev.mcspring.student.model.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    List<Student> findAllBySchoolId(Integer schoolId);
+}
