31a55ab18239a2e701f896a08a58792e91565d0d
vitalvirtue <hyterdem@hotmail.com>
2024-06-07 11:43:44 +0300

    Student Services and its Model

---
+++ student/src/main/java/dev/mcspring/student/model/Student.java
@@ -0,0 +1,23 @@
+package dev.mcspring.student.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Integer schoolId;
+}
